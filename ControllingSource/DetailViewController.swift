283d08901a9b110be59ebce7d56414c064b9abaf
mario <mariogf94@gmail.com>
2015-08-31 15:57:28 -0300

    Revert "Revert "commit para teste blame""
    This reverts commit 956b7f70b1c92607c039b7f396ac5d1c6f43c441.

--- ControllingSource/DetailViewController.swift
+++ ControllingSource/DetailViewController.swift
@@ -34,11 +34,11 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
